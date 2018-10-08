86232e38219a14202b0e3e5f009b23184d714ead
Eleanor Jones <Eleanor.Jones.1@warwick.ac.uk>
2018-10-08 09:26:02 +0100

    changed main file

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,1 +1,17 @@
-/ / mpags-cipher.cpp
+#include <iostream>
+#include <string>
+int main()
+{
+  char in_char{'x'};
+  std::cout<<"Type the input; press ctrl+d when finished"<< "\n";
+  std::
+  while(std::cin>>in_char)
+    {switch(in_char)
+      case 0:
+	std::string
+
+      putchar (toupper(in_char));
+	}
+  return 0;
+      
+}
